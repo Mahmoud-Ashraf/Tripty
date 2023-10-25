2d07b9430a4cbafbb63c86ebb2fa6525a7b31216
MahmoudAshrafTaha <145581054+MahmoudAshrafTaha@users.noreply.github.com>
2023-10-26 01:01:53 +0300

    remove comments

--- components/HomeReviews/HomeReviews.tsx
+++ components/HomeReviews/HomeReviews.tsx
@@ -16,45 +16,19 @@ const HomeReviews = () => {
     return (
         <>
             <SectionHeader title="Happy Customers" />
-            {/* <div className="row flex-nowrap my-5">
-                <div className="col-7">
-                </div>
-                <div className="col-7">
-                    <Review review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis cras sed. Viverra ipsum nunc aliquet bibendum enim. Risus pretium quam" user={{ name: 'mahmoud taha', title: 'frontend Developer', avatar: '@/public/assets/images/logo.svg' }} />
-                </div>
-            </div> */}
             <Slider {...settings}>
                 <div>
                     <Review review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis cras sed. Viverra ipsum nunc aliquet bibendum enim. Risus pretium quam" user={{ name: 'mahmoud taha', title: 'frontend Developer', avatar: '@/public/assets/images/logo.svg' }} />
-                    {/* <h3>1</h3> */}
                 </div>
                 <div>
                     <Review review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis cras sed. Viverra ipsum nunc aliquet bibendum enim. Risus pretium quam" user={{ name: 'mahmoud taha', title: 'frontend Developer', avatar: '@/public/assets/images/logo.svg' }} />
-                    {/* <h3>1</h3> */}
                 </div>
                 <div>
                     <Review review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis cras sed. Viverra ipsum nunc aliquet bibendum enim. Risus pretium quam" user={{ name: 'mahmoud taha', title: 'frontend Developer', avatar: '@/public/assets/images/logo.svg' }} />
-                    {/* <h3>1</h3> */}
                 </div>
                 <div>
                     <Review review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis cras sed. Viverra ipsum nunc aliquet bibendum enim. Risus pretium quam" user={{ name: 'mahmoud taha', title: 'frontend Developer', avatar: '@/public/assets/images/logo.svg' }} />
-                    {/* <h3>1</h3> */}
                 </div>
-                {/* <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div> */}
             </Slider>
         </>
     )
