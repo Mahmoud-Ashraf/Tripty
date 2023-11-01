55732e128a5122405c13b46e4ed050de453940fe
Mahmoud Ashraf <mahmoud.ash.07@gmail.com>
2023-11-01 22:28:56 +0200

    implement trend now page

---
+++ pages/trend/index.tsx
@@ -0,0 +1,59 @@
+import HomeTabs from '@/components/HomeTabs/HomeTabs';
+import classes from './trend.module.scss';
+import { Place } from '@/interfaces/place';
+import { Category } from '@/interfaces/category';
+import PlaceHeader from '@/components/UI/PlaceHeader/PlaceHeader';
+
+interface CategorizedPlaces {
+    [categoryName: string]: Place[]; // Define the structure for categorized places
+}
+
+interface Props {
+    tabs: Category[] | [],
+    categorizedPlaces: CategorizedPlaces
+}
+
+const Trend = (props: Props) => {
+    const { tabs, categorizedPlaces } = props;
+
+    return (
+        <div className={classes.container}>
+            <PlaceHeader name='Trend Now' />
+            <HomeTabs tabs={tabs} categorizedPlaces={categorizedPlaces} />
+        </div>
+    )
+}
+
+
+export async function getStaticProps() {
+    try {
+        const categoriesReq = await fetch('http://18.133.139.168/api/v1/front/categories');
+        const categoriesData = await categoriesReq.json();
+        categoriesData.data.unshift({ name: 'all', id: 0, icon: '' });
+
+        const categorizedPlaces: CategorizedPlaces = {}; // Initialize as the defined interface
+
+        await Promise.all(categoriesData.data.map(async (category: any) => {
+            const categoryPlacesReq = await fetch(`http://18.133.139.168/api/v1/front/places?trend_now=1&category_id=${category.id}`);
+            const categoryPlacesData = await categoryPlacesReq.json();
+
+            categorizedPlaces[category.name] = categoryPlacesData.data;
+        }));
+        return {
+            props: {
+                tabs: categoriesData.data,
+                categorizedPlaces
+            }
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            props: {
+                tabs: [],
+                categorizedPlaces: {} as CategorizedPlaces, // Initialize as the defined interface
+            }
+        };
+    }
+}
+
+export default Trend;
