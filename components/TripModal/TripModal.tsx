6c375cac487c2be9e5cde1a23641a7317321673d
Mahmoud Ashraf <mahmoud.ash.07@gmail.com>
2023-11-06 23:42:57 +0200

    add trip modal footer

--- components/TripModal/TripModal.tsx
+++ components/TripModal/TripModal.tsx
@@ -1,9 +1,11 @@
+import TripModalFooter from './TripModalFooter';
 import TripModalHeader from './TripModalHeader';
 import classes from './trip-modal.module.scss';
 const TripModal = () => {
-    return(
+    return (
         <div className={classes.container}>
             <TripModalHeader />
+            <TripModalFooter />
         </div>
     )
 }
---
+++ components/TripModal/TripModalFooter.tsx
@@ -0,0 +1,16 @@
+const TripModalFooter = () => {
+    return (
+        <div>
+            <div className="row justify-content-end mt-5">
+                <div className="col-4">
+                    <button className="btn btn-main w-100">Cancel</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-main w-100">Next</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TripModalFooter;
