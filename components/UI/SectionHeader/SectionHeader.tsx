d948b7eb5d9150438bcbb5cb900ddeb8a5b829b2
Mahmoud Ashraf <mahmoud.ash.07@gmail.com>
2023-12-13 00:24:20 +0200

    fix build error

--- components/UI/SectionHeader/SectionHeader.tsx
+++ components/UI/SectionHeader/SectionHeader.tsx
@@ -2,11 +2,11 @@ import Translate from '@/components/helpers/Translate/Translate';
 import classes from './section-header.module.scss';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-const SectionHeader = ({ title, icon, onIconClick }: { title: string, icon?: any, onIconClick: string }) => {
+const SectionHeader = ({ title, icon, onIconClick }: { title: string, icon?: any, onIconClick?: string }) => {
     const router = useRouter();
     return (
         <header className={classes.container}>
-            <h2 className={classes.header}><Translate id={title} /> {icon && <Image onClick={() => router.push(onIconClick)} alt={title} src={icon} />}</h2>
+            <h2 className={classes.header}><Translate id={title} /> {icon && <Image onClick={() => router.push(onIconClick || '')} alt={title} src={icon} />}</h2>
         </header>
     )
 }
