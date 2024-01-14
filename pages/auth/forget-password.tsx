99cf748c6385e124fda7942dfaa11bd325047c4d
Mahmoud Ashraf <mahmoud.ash.07@gmail.com>
2024-01-14 02:13:24 +0200

    add reset password

--- pages/auth/forget-password.tsx
+++ pages/auth/forget-password.tsx
@@ -7,26 +7,27 @@ import { Button, Form } from 'react-bootstrap';
 import Translate from '@/components/helpers/Translate/Translate';
 import useTranslate from '@/hooks/use-translate';
 import AuthLayout from '@/components/layout/AuthLayout/AuthLayout';
+import useHTTP from '@/hooks/use-http';
 
 const ForgetPassword = (props: any) => {
     const { sliders } = props;
     const { translate } = useTranslate();
+    const { isLoading, error, sendRequest } = useHTTP();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loginError, setLoginError] = useState('');
-    const handleSignIn = async () => {
-        signIn('credentials', {
-            redirect: false,
-            email: email,
-            password: password,
-        }).then(data => {
-            if (data?.error) {
-                setLoginError('errors.wrongCredentials');
-            } else {
-                setLoginError('');
-            }
-        });
-
+    const handleForgetPassword = async () => {
+        if (email) {
+            sendRequest(
+                {
+                    url: '/api/user/forgetPassword',
+                    method: 'POST',
+                    body: { email }
+                },
+                (data: any) => console.log(data),
+                (err: any) => console.error(err)
+            )
+        }
     };
     return (
         <>
@@ -38,7 +39,7 @@ const ForgetPassword = (props: any) => {
                     <h1><Translate id='auth.enterEmail' /></h1>
                     <input className={classes.input} type="email" placeholder={translate('placeholder.email')} value={email} onChange={(e) => setEmail(e.target.value)} />
                     <div className={`d-grid gap-2 ${classes.submit}`}>
-                        <Button variant="main" type="button" onClick={handleSignIn}>
+                        <Button variant="main" type="button" onClick={handleForgetPassword}>
                             <Translate id='buttons.send' />
                         </Button>
                     </div>
