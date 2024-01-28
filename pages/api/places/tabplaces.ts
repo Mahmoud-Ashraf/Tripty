6dfc48cba81693b60054146e42a0e64a8d5bdd20
Mahmoud Ashraf <mahmoud.ash.07@gmail.com>
2024-01-28 23:34:59 +0200

    fix home places number to 40

--- pages/api/places/tabplaces.ts
+++ pages/api/places/tabplaces.ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { locale, trendNow, long, lat, categoryId, text } = req.query;
     try {
         const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-        const placesReq = await fetch(`${baseUrl}places?per_page=20&change_language=${locale}${!!long ? `&long=${long}` : ''}${!!lat ? `&lat=${lat}` : ''}&category_id=${categoryId}${!!trendNow ? '&trend_now=1' : ''}${!!text ? `&text=${text}` : ''}`);
+        const placesReq = await fetch(`${baseUrl}places?per_page=40&change_language=${locale}${!!long ? `&long=${long}` : ''}${!!lat ? `&lat=${lat}` : ''}&category_id=${categoryId}${!!trendNow ? '&trend_now=1' : ''}${!!text ? `&text=${text}` : ''}`);
         if (!placesReq.ok) {
             throw new Error('error fetching tab places');
         }
